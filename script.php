0c36bafe25d64b0b2529fb3ba1d3eb782b882f1b
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-27 13:21:14 +0200

    add cookies

--- script.php
+++ script.php
@@ -3,5 +3,9 @@ function MSG() {
 echo "Today is " . date("d/m/Y") . "<br>";
 }
 MSG(); // call the function
-
+$cookie_name = "user";
+$cookie_value = "John Smith";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); 
+//Cookies are used to identify a user
+//A cookie is created with the setcookie() function
 ?>
