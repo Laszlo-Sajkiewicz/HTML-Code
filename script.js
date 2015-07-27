d186987221c7393c356189d45a8046057ff17d8f
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-27 10:04:20 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+
+function open_info()
+{
+window.open('index.html','nom_de_ma_popup','menubar=no, scrollbars=no, top=100, left=100, width=300, height=200');
+}
+
+</script>
