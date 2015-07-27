cdb8a57bf9a576cc0fed11da8220c34f553b9bf1
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-27 12:19:05 +0200

    add function but not testing

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ function open_info()
 window.open('index.html','nom_de_ma_popup','menubar=no, scrollbars=no, top=100, left=100, width=300, height=200');
 }
 
+
+$(function(){
+$("body").createFlashMessage({
+type: 'success',
+message: '<b>Well done!</b>',
+closeable: true,
+permanent: true,
+position: 'static'
+});
 </script>
