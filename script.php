5459f91763c80ac441995d8cd851f0b34683d55f
Laszlo Sajkiewicz <laszlo-94@hotmail.fr>
2015-07-29 09:11:58 +0200

    add email validation

--- script.php
+++ script.php
@@ -8,4 +8,10 @@ $cookie_value = "John Smith";
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); 
 //Cookies are used to identify a user
 //A cookie is created with the setcookie() function
+
+        $mail='john.smith@linux.fr' ;
+        echo "* $mail\t > Adresse ".((strlen(filter_var($mail, FILTER_VALIDATE_EMAIL))) ? "valide" : "invalide")."\n" ;
+        $mail='john.smith@-linux' ;
+        echo "* $mail\t > Adresse ".((strlen(filter_var($mail, FILTER_VALIDATE_EMAIL))) ? "valide" : "invalide")."\n" ;
 ?>
+
